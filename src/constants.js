938e4c532ae977f58b643cc3216d4333606331ec
Matt Lilley <thetrime@gmail.com>
2016-04-16 07:40:55 +1200

    Add cut

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ var AtomTerm = require('./atom_term.js');
 var Functor = require('./functor.js');
 
 module.exports = {emptyListAtom: AtomTerm.get("[]"),
+		  cutAtom: AtomTerm.get("!"),
+
 		  clauseFunctor: Functor.get(AtomTerm.get(":-"), 2),
 		  conjunctionFunctor: Functor.get(AtomTerm.get(","), 2),
 		  disjunctionFunctor: Functor.get(AtomTerm.get(";"), 2),
