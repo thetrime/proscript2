002f58405ab581991f4cacef786d9ed9a819d636
Matt Lilley <thetrime@gmail.com>
2016-07-14 01:51:30 +1200

    Add some profiling info to runtime now I am focusing on performance

--- src/test_node.js
+++ src/test_node.js
@@ -1,11 +1,13 @@
 var Prolog = require('./core');
 var env = new Prolog.Environment();
-var test_file = "inriasuite.pl";
-//var test_file = "tests/t.pl";
+var test_file = "tests/t.pl";
 env.consultFile(test_file, function()
-               {
+                {
+                    var d0 = Date.now();
                    if (!env.execute(new Prolog.AtomTerm("run_all_tests")))
                        console.log("Failed");
                    else
                        console.log("Succeeded");
+                    var d1 = Date.now();
+                    console.log("Execution (not compilation) time: " + (d1 - d0)/1000 + "s");
                });
