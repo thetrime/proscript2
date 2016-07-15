44a19f05deb849366e0a555e90a642b95f9c1de7
Matt Lilley <thetrime@gmail.com>
2016-07-15 18:16:13 +1200

    Expose Errors and BlobTerm

--- src/core.js
+++ src/core.js
@@ -2,8 +2,10 @@
 
 module.exports = {Environment: require('./environment.js'),
                   AtomTerm: require('./atom_term.js'),
+                  BlobTerm: require('./blob_term.js'),
                   Utils: require('./utils.js'),
                   VariableTerm: require('./variable_term.js'),
                   CompoundTerm: require('./compound_term.js'),
                   Constants: require('./constants.js'),
+                  Errors: require('./errors.js'),
                   Functor: require('./functor.js')};
