548d3357b294cf0435f11507d30db048d3345645
Matt Lilley <thetrime@gmail.com>
2016-07-19 18:44:33 +1200

    Adjust inriasuite to work with yielding

--- tests/inriasuite/inriasuite.js
+++ tests/inriasuite/inriasuite.js
@@ -1,10 +1,22 @@
 var Prolog = require('../../src/core');
 var env = new Prolog.Environment();
 var test_file = "inriasuite.pl";
+
+
+
 env.consultFile(test_file, function()
                {
-                   if (!env.execute(new Prolog.AtomTerm("run_all_tests")))
-                       console.log("Failed");
-                   else
-                       console.log("Succeeded");
+                   env.execute(new Prolog.AtomTerm("run_all_tests"),
+                               function()
+                               {
+                                   console.log("Succeeded");
+                               },
+                               function()
+                               {
+                                   console.log("Failed");
+                               },
+                               function(e)
+                               {
+                                   console.log("Error: " + e.toString());
+                               });
                });
