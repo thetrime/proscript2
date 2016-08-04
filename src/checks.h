3b5dd127e2cc2c6c6afd73ec2f94e8b402af30a9
Matt Lilley <thetrime@gmail.com>
2016-08-05 04:36:05 +1200

    oops. That was a lot of mistakes.

--- src/checks.h
+++ src/checks.h
@@ -1,7 +1,8 @@
 #include "errors.h"
 
-#define must_be_bound(a) ((TAGOF(a) != VARIABLE_TAG) || SET_EXCEPTION(instantiation_error()))
-#define must_be_atom(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == ATOM_TYPE) || SET_EXCEPTION(type_error(atomAtom, a)))
-#define must_be_positive_integer(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == INTEGER_TYPE && getConstant(a).data.integer_data->data >= 0) || SET_EXCEPTION(domain_error(notLessThanZeroAtom, a)))
-#define must_be_integer(a) (TAGOF(a) == CONSTANT_TAG && getConstant(a).type == INTEGER_TYPE || SET_EXCEPTION(type_error(integerAtom, a)))
-#define must_be_character(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == ATOM_TYPE && getConstant(a).data.atom_data->length == 1) || SET_EXCEPTION(type_error(characterAtom, a)))
+#define must_be_bound(a) ((TAGOF(a) != VARIABLE_TAG) || instantiation_error())
+#define must_be_atom(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == ATOM_TYPE) || type_error(atomAtom, a))
+#define must_be_positive_integer(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == INTEGER_TYPE && getConstant(a).data.integer_data->data >= 0) || domain_error(notLessThanZeroAtom, a))
+#define must_be_integer(a) (TAGOF(a) == CONSTANT_TAG && getConstant(a).type == INTEGER_TYPE || type_error(integerAtom, a))
+#define must_be_character(a) ((TAGOF(a) == CONSTANT_TAG && getConstant(a).type == ATOM_TYPE && getConstant(a).data.atom_data->length == 1) || type_error(characterAtom, a))
+#define must_be_predicate_indicator(a) ((TAGOF(a) == COMPOUND_TAG && FUNCTOROF(a) == predicateIndicatorFunctor && TAGOF(ARGOF(a, 0)) == CONSTANT_TAG && getConstant(TAGOF(ARGOF(a, 0))).type == ATOM_TYPE && TAGOF(ARGOF(a, 1)) == CONSTANT_TAG && getConstant(TAGOF(ARGOF(a, 1))).type == INTEGER_TYPE) || type_error(predicateIndicatorAtom, a))
